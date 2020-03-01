b005ea6ed3d20e02cbd43c215418fff45bed0c8f
James Johnson <music1384@yahoo.co.uk>
2020-03-01 21:49:44 +0000

    Update folder structure

---
+++ Soul Engine - Prototype/Assets/Code/Classes/Components/CullComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SoulEngine
+{
+	[RequireComponent (typeof (Collider2D))]
+	public class CullComponent : MonoBehaviour
+	{
+		private void Awake ()
+		{
+			GetComponent<Collider2D> ().isTrigger = true;
+		}
+
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
