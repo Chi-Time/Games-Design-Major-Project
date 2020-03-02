5abf98fd5f0080fc850de41573fbe97c88526101
James Johnson <music1384@yahoo.co.uk>
2020-03-02 20:25:15 +0000

    Create globals and set dress test scene

---
+++ Soul Engine - Prototype/Assets/Code/Classes/Globals.cs
@@ -0,0 +1,16 @@
+﻿using Code.Enums;
+
+namespace Code.Classes
+{
+	public static class Globals
+	{
+		/// <summary>The standard time scale that the game operates at.</summary>
+		public static readonly float StandardTimeScale = 1.0f;
+		/// <summary>The timescale the game runs at when slowed.</summary>
+		public static readonly float SlowedTimeScale = 0.75f;
+		/// <summary>The timescale the game runs at when boosted.</summary>
+		public static readonly float BoostTimeScale = 1.35f;
+		/// <summary>The current difficulty mode of the game.</summary>
+		public static Diificulty CurrentDifficulty = Diificulty.Normal;
+	}
+}
