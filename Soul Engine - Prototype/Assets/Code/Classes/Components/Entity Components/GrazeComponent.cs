095a56c5f117bc343d63d8d0b9f97f2de2ee6829
James Johnson <music1384@yahoo.co.uk>
2020-04-05 19:20:49 +0100

    Refactor graze component, implement outline component

--- Soul Engine - Prototype/Assets/Code/Classes/Components/Entity Components/GrazeComponent.cs
+++ Soul Engine - Prototype/Assets/Code/Classes/Components/Entity Components/GrazeComponent.cs
@@ -4,6 +4,7 @@ using Utilities;
 
 namespace SoulEngine
 {
+	[RequireComponent (typeof (OutlineComponent))]
 	public class GrazeComponent : MonoBehaviour
 	{
 		[Header ("Graze Settings")]
@@ -13,26 +14,17 @@ namespace SoulEngine
 		private float _ScoreInterval = 0.0f;
 		[Tooltip ("The radius around the enemy for when to award score."), SerializeField]
 		private float _GrazeRadius = 2f;
-		
-		[Header ("Outline Settings")]
-		[Tooltip ("The object to use to display an outline"), SerializeField]
-		private SpriteRenderer _OutlinePrefab = null;
-		[Tooltip ("How strong the alpha is when visible."), SerializeField]
-		private float _OutlineVisible = 0.5f;
-		[Tooltip ("How low the alpha is when invisible"), SerializeField]
-		private float _OutlineInvisible = 0.0f;
-		
+
+		/// <summary>Detection circle around this object.</summary>
 		private Circle _GrazeCircle;
 		/// <summary>Reference to the player position.</summary>
 		private Transform _Target = null;
 		/// <summary>Reference to this object's transform component.</summary>
 		private Transform _Transform = null;
+		/// <summary>Reference to the outline component on this object.</summary>
+		private OutlineComponent _Outline = null;
 		/// <summary>Regulator for awarding score at intervals.</summary>
 		private Regulator _ScoringRegulator = null;
-		/// <summary>Transform component of graze outline.</summary>
-		private Transform _OutlineTransform = null;
-		/// <summary>Renderer component of graze outline.</summary>
-		private SpriteRenderer _OutlineRenderer = null;
 
 		private void Awake ()
 		{
@@ -45,11 +37,8 @@ namespace SoulEngine
 
 		private void SetupOutline ()
 		{
-			_OutlineRenderer = Instantiate (_OutlinePrefab, _Transform, false);
-			_OutlineTransform = _OutlineRenderer.GetComponent<Transform> ();
-			
-			_OutlineTransform.localScale = new Vector3 (_GrazeRadius, _GrazeRadius, 1.0f);
-			_OutlineRenderer.color = _OutlineRenderer.color.Alpha (0.0f);
+			_Outline = GetComponent<OutlineComponent> ();
+			_Outline.Resize (_GrazeRadius);
 		}
 
 		private void Start ()
@@ -59,21 +48,30 @@ namespace SoulEngine
 
 		private void Update ()
 		{
+			CheckIntersections ();
+		}
+
+		private void CheckIntersections ()
+		{
 			// Are we intersecting with the target?
 			if (_GrazeCircle.IsIntersecting (_Target))
 			{
 				// Show the graze outline.
-				_OutlineRenderer.color = _OutlineRenderer.color.Alpha (_OutlineVisible);
-				
-				// If we can increase the score, then do so.
-				if (_ScoringRegulator.HasElapsed (true))
-					LevelSignals.OnScoreIncreased?.Invoke (_Score);
+				_Outline.Show (true);
+				UpdateScore ();
 			}
 			else
 			{
 				// Hide the graze outline.
-				_OutlineRenderer.color = _OutlineRenderer.color.Alpha (_OutlineInvisible);
+				_Outline.Show (false);
 			}
+		}
+
+		private void UpdateScore ()
+		{
+			// If we can increase the score, then do so.
+			if (_ScoringRegulator.HasElapsed (true))
+				LevelSignals.OnScoreIncreased?.Invoke (_Score);
 		}
 	}
 }
---
+++ Soul Engine - Prototype/Assets/Code/Classes/Components/Entity Components/OutlineComponent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace SoulEngine
+{
+	[RequireComponent (typeof (SpriteRenderer))]
+	public class OutlineComponent : MonoBehaviour
+	{
+		[Tooltip ("The strength of the outline when enabled"), SerializeField, Range (0.0f, 1.0f)]
+		private float _EnabledStrength = 1.0f;
+		[Tooltip ("The strength of the outline when disabled"), SerializeField, Range (0.0f, 1.0f)]
+		private float _DisabledStrength = 0.0f;
+
+		private Transform _Transform = null;
+		private SpriteRenderer _Renderer = null;
+
+		private void Awake ()
+		{
+			_Transform = GetComponent<Transform> ();
+			_Renderer = GetComponent<SpriteRenderer> ();
+		}
+
+		/// <summary>Resizes the outline to the given size.</summary>
+		/// <param name="size">The size of the outline in the world.</param>
+		public void Resize (float size)
+		{
+			_Transform.localScale = new Vector3 (size, size, size);
+		}
+
+		/// <summary>Shows/Hides the outline.</summary>
+		/// <param name="shouldDisplay">Should the outline be displayed?</param>
+		public void Show (bool shouldDisplay)
+		{
+			if (shouldDisplay)
+				_Renderer.color = _Renderer.color.Alpha (_EnabledStrength);
+			else
+				_Renderer.color = _Renderer.color.Alpha (_DisabledStrength);
+		}
+	}
+}
